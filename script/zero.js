133cf0c6c6508c6fa8f7c0e577ae95ae37362089
Esa Koskinen <esa.koskinen@reaktor.co.jp>
2018-12-02 12:54:09 +0900

    Added a LOT OF NEW STUFF to pollute the source trees and cause a premature end for all of civilization.

---
+++ script/zero.js
@@ -0,0 +1,21 @@
+// Okay, let's do things more properly this time around.
+
+// We can pull together and get the better of this worl'd s wikced aura thing majhababings.
+
+// Lets go...
+
+(function() {
+
+    gameState = {
+
+    }
+
+    // THE BEAUTY OF CULTURE AND EARTH
+    function handleCalligraphy() {
+        
+    }
+
+
+    // Export stuff overseas
+    window.onload = handleCalligraphy
+})()
